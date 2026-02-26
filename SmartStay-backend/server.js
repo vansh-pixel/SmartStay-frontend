7a0569b664e0d448a554325fb3930283af726177
vansh-pixel <vanshmamtora17@gmail.com>
2026-02-26 10:18:57 +0530

    fix(cors): allow CLIENT_URL for frontend requests

--- SmartStay-backend/server.js
+++ SmartStay-backend/server.js
@@ -18,7 +18,13 @@ const PORT = process.env.PORT || 5000;
 
 //----MIDDLEWARE----
 app.use(cors({
-  origin: ["http://localhost:3000", "http://127.0.0.1:3000", "http://localhost:3001", "http://127.0.0.1:3001"],
+  origin: [
+    "http://localhost:3000", 
+    "http://127.0.0.1:3000", 
+    "http://localhost:3001", 
+    "http://127.0.0.1:3001",
+    process.env.CLIENT_URL
+  ].filter(Boolean),
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization']
